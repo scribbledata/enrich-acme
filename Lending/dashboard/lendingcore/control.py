e3884c32ff8785a174d7aaca82f2c00df9c2a798
Venkata Pingali <pingali@gmail.com>
2020-04-24 09:00:45 +0530

    Updated control panel

--- Lending/dashboard/lendingcore/control.py
+++ Lending/dashboard/lendingcore/control.py
@@ -1,13 +1,78 @@
 from enrichapp.dashboard.campaigns import lib as scribblelib
 
 overview_spec = {
-    'name': 'Lending ML Plaform',
-    'description': "Implement and Operate ML Engineering services",
-    "details": """<p class='text-center'>This dashboard gives an overview of the services enabled on this platform. Click on each tab.</p>
+    'name': 'Acme Intelligence Platform (AIP)',
+    'description': "Acme-wide Machine Learning and Advanced Analytics Platform",
 
-<p class='text-center'>Please reachout to <a href='mailto:support@scribbledata.io'>Scribble Support</a> for help</p>""",
+    "details": """<p class='text-center'>This dashboard gives an overview of the services enabled on AIP. Click on each tab to see details. AIP's purpose is to enable strong and continuous model-driven operations.</p>
+
+    <p class='text-center'>AIP Product Owner is <a href='mailto:bill.preston@acmeinc.com'>Bill Preston</a>, and is managed by <a href='mailto:sam.jacobs@acmeinc.com'>Sam Jacobs</a>. They are being helped by <a href="https://www.scribbledata.io">Scribble Team</a>.</p>
+
+<p class='text-center'>Please reachout to <a href='mailto:aip@acmeinc.com'>AIP Team</a> or <a href='mailto:support@scribbledata.io'>Scribble Support</a> for technical help</p>""",
     'customer': scribblelib.find_my_customer(__file__),
     'force_list': [],
+    'usecases': [ 
+        {
+            'name': "Scoring",
+            'category': 'Product Listing',
+            'owner': 'Mitul/John',
+            'description': 'Score products on various criteria',
+            'frequency': 'Daily',
+            'services': ['Pipeline', 'Notebook', 'MLFlow'],
+            'datasets': ['ProductPersona', 'Event'],
+            'status': 'Production'
+        },
+        {
+            'name': "Policy Selection",
+            'category': 'Product Listing',
+            'owner': 'Mitul/John',
+            'description': 'A/B testing with various configurations and levers',
+            'frequency': 'Adhoc',
+            'services': ['Notebook'],
+            'datasets': ['ProductPersona', 'Events'],
+            'status': 'Ongoing'
+        },
+        {
+            'name': "LTV",
+            'category': 'Customer',
+            'owner': 'Valerie',
+            'description': 'LTV of Customers',
+            'services': ['Pipeline', 'Notebook'],
+            'datasets': ['CustomerPersona'],
+            'status': 'Staging',
+            'frequency': 'Weekly'
+        },
+        {
+            'name': "RiskModel",
+            'category': 'Customer',
+            'owner': 'Valerie',
+            'description': 'Risk assessment for each customer',
+            'services': ['Pipeline', 'Notebook'],
+            'datasets': ['CustomerPersona'],
+            'status': 'Staging',
+            'frequency': 'Weekly'
+        },        
+        {
+            'name': "Next Visit",
+            'category': 'Customer',
+            'owner': 'Valerie',
+            'description': 'Predict the next visit and probable need',
+            'services': ['Pipeline', 'Notebook'],
+            'datasets': ['TransactionProfile'],
+            'status': 'Production',
+            'frequency': 'Daily'
+        },
+        {
+            'name': "Data Drift Monitoring",
+            'category': 'Data Quality',
+            'owner': 'Valerie',
+            'description': 'Assess changes in scores',
+            'services': ['Pipeline', 'Notebook'],
+            'datasets': ['Drifter'],
+            'status': 'Production',
+            'frequency': 'Daily'
+        },
+    ],
     'implement': [
         {
             'label': 'Catalog',
@@ -39,9 +104,14 @@ overview_spec = {
     ],
     "audit": [
         {
-            'label': 'Lineage (Beta)',
+            'label': 'Provenance',
             'url': 'lendingcore:audit:index',
-            'description': "Search interface for platform artifacts",
+            'description': "Lineage each dataset & Dataset Inventory",
+        },
+        {
+            'label': "Compare",
+            'url': 'lendingcore:compare:index',
+            'description': 'Compare pipeline runs across days'
         },
     ],
     "access": [
@@ -56,10 +126,17 @@ overview_spec = {
             'url': 'lendingcore:loan:index'
         },        
         {
-            'label': 'Feature Service',
+            'label': 'Feature Serve',
             'description': "Features at Point in Time",
             'url': 'lendingcore:featureserve:index'
         },        
+    ],
+    "monitor": [
+        {
+            'label': 'Data Drift',
+            'url': 'lendingcore:marketplace:index',
+            'description': 'Compute changes in data',
+        }
     ]
 }
 
